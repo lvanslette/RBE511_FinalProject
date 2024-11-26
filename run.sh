5d65a0734d07353122b3cb53f9bd43f4d3bfd366
Lucas Vanslette <lvanslette2@gmail.com>
2024-11-26 14:06:17 -0800

    using hw9.bzz and movement.argos, creating starting environment for Final Project

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/lvanslette/WPI/RBE511_HW10/build
+cd /home/lvanslette/WPI/RBE511_FinalProject/build
 make -j8
 cd ..
-argos3 -c threshold_model.argos
+argos3 -c movement.argos
 echo "copied data.dat to /mnt/hgfs/Shared"
-cp ~/WPI/RBE511_HW10/data.dat /mnt/hgfs/Shared/
+cp ~/WPI/RBE511_FinalProject/data.dat /mnt/hgfs/Shared/
