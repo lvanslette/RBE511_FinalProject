0f9854f18769fe2629e73aedd186f88b049f6207
Lucas Vanslette <lvanslette2@gmail.com>
2024-11-26 17:02:54 -0800

    updated run script

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-cd /home/lvanslette/WPI/RBE511_FinalProject/build
+if [ ! -d "build" ]; then
+   mkdir build
+   cd build
+   cmake ..
+   cd ..
+fi
+cd build
 make -j8
 cd ..
 argos3 -c movement.argos
-echo "copied data.dat to /mnt/hgfs/Shared"
-cp ~/WPI/RBE511_FinalProject/data.dat /mnt/hgfs/Shared/
